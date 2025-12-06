a97ab0206cf451697bc93695410b50191b0c95e4
Rourugin <maks.4min@gmail.com>
2025-12-06 23:03:00 +0300

    requirements and constructions

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -7,6 +7,6 @@ from app.handlers.constructions_h import constructions_r
 def setup_routers() -> Router:
     router = Router()
     router.include_routers(main_r)
-    router.include_routers(verbs_r)
     router.include_routers(constructions_r)
+    router.include_routers(verbs_r)
     return router
